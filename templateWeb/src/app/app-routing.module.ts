4e10533a11800066e3e6695707d007e5a87d6407
DudyDudy <ndubourg1991@gmail.com>
2018-10-17 19:19:31 +0200

    fix : localhost:4200 s'ouvre bien sur home.component (via routing)

--- templateWeb/src/app/app-routing.module.ts
+++ templateWeb/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ArticlesComponent } from './articles/articles.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'articles', component: ArticlesComponent },
   { path: 'home', component: HomeComponent}
 ];
