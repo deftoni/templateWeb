33b03674e6aaec2002a292c64bf9f1538babeb37
deftoni <deftoni59@gmail.com>
2018-10-26 17:07:04 +0200

    Check2

--- templateWeb/src/app/articles/mock-articles.ts
+++ templateWeb/src/app/articles/mock-articles.ts
@@ -6,5 +6,6 @@ export const ARTICLES: Article[] = [
   { id: '7547', title: 'plopo', content: 'Bombasto' },
   { id: '8566', title: 'plopu', content: 'Celeritas' },
   { id: '9785', title: 'plopa', content: 'Magneta' },
+  { id: '9659', title: 'plopp', content: 'RubberWoman' },
   { id: '9654', title: 'plopy', content: 'RubberMan' }
 ];
