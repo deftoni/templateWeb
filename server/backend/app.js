a8c172560d73269a11189e0c50b74d3ba3b8b183
deftoni <deftoni59@gmail.com>
2018-11-04 09:16:12 +0100

    Architecture Backend

--- server/backend/app.js
+++ server/backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Article = require('./models/article');
+const articlesRoutes = require('./routes/articles');
 
 const app = express();
 
@@ -31,46 +31,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/articles', (req, res, next) => {
-  const article = new Article({
-    title: req.body.title,
-    content: req.body.content
-  });
-  article.save().then(createdArticle => {
-    res.status(201).json({
-      message: 'Article added successfully',
-      articleId: createdArticle._id
-    });
-  });
-});
-
-app.get('/api/articles', (req, res, next) => {
-  Article.find()
-    .then(documents => {
-      res.status(200).json({
-        message: 'articles fetched successfully',
-        articles: documents
-      });
-    });
-});
-
-app.delete('/api/articles/:id', (req, res, next) => {
-  Article.deleteOne({ _id: req.params.id })
-    .then(() => {
-      res.status(200).json({
-        message: 'Article deleted successfully'
-      });
-    });
-});
-
-app.put('/api/articles/:id', (req, res, next) => {
-  Article.findByIdAndUpdate({ _id: req.params.id }, req.body).then(updatedArticle => {
-    console.log(updatedArticle);
-    res.status(201).json({
-      message: 'Article updated successfully',
-      articleId: updatedArticle._id
-    });
-  });
-});
+app.use('/api/articles', articlesRoutes);
 
 module.exports = app;
---
+++ server/backend/routes/articles.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const Article = require('../models/article');
+
+const router = express.Router();
+
+router.post('', (req, res, next) => {
+    const article = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+    article.save().then(createdArticle => {
+      res.status(201).json({
+        message: 'Article added successfully',
+        articleId: createdArticle._id
+      });
+    });
+  });
+  
+  router.get('', (req, res, next) => {
+    Article.find()
+      .then(documents => {
+        res.status(200).json({
+          message: 'articles fetched successfully',
+          articles: documents
+        });
+      });
+  });
+  
+  router.delete('/:id', (req, res, next) => {
+    Article.deleteOne({ _id: req.params.id })
+      .then(() => {
+        res.status(200).json({
+          message: 'Article deleted successfully'
+        });
+      });
+  });
+  
+  router.put('/:id', (req, res, next) => {
+    Article.findByIdAndUpdate({ _id: req.params.id }, req.body).then(updatedArticle => {
+      console.log(updatedArticle);
+      res.status(201).json({
+        message: 'Article updated successfully',
+        articleId: updatedArticle._id
+      });
+    });
+  });
+
+  module.exports = router;
